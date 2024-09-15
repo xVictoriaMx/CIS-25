d35d396834a921916e614d27ef4915aa806531d5
xVictoriaMx <156991042+xVictoriaMx@users.noreply.github.com>
2024-09-15 14:05:26 -0700

    Assignment 5

---
+++ Assignments/Assignment_5/main.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <limits>   
+
+using namespace std;
+
+// Size and Range of data types
+int main() {
+    cout << "Size of short: " << sizeof(short) << " byte" << endl;
+    cout << "Range of short: " << SHRT_MIN << " to " << SHRT_MAX << endl;
+
+    cout << "\nSize of double: " << sizeof(double) << " byte" << endl;
+    cout << "Range of double: " << numeric_limits<double>::min() << " to " << numeric_limits<double>::max() << endl;
+
+    cout << "\nSize of int: " << sizeof(int) << " byte" << endl;
+    cout << "Range of int: " << INT_MIN << " to " << INT_MAX << endl;
+
+    cout << "\nSize of float: " << sizeof(float) << " byte" << endl;
+    cout << "Range of float: " << numeric_limits<float>::min() << " to " << numeric_limits<float>::max() << endl;
+
+    cout << "\nSize of char: " << sizeof(char) << " byte" << endl;
+    cout << "Range of char: " << CHAR_MIN << " to " << CHAR_MAX << endl;
+
+    cout << "\nSize of unsigned int: " << sizeof(unsigned int) << " byte" << endl;
+    cout << "Range of unsigned int: 0 to " << UINT_MAX << endl;
+
+    return 0;
+}
