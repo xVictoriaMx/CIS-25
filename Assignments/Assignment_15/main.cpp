48f1be6d5ccd1dc8ca4e6785ac550ba5ba00c1bb
xVictoriaMx <156991042+xVictoriaMx@users.noreply.github.com>
2024-09-23 11:12:27 -0700

    CHange variables from i and j, to x and y

--- Assignments/Assignment_15/main.cpp
+++ Assignments/Assignment_15/main.cpp
@@ -9,8 +9,8 @@ int main() {
 
     cin >> rows;
 
-    for (int i =1; i<=rows; i++) {
-        for (int j = 1; j<=i; j++)
+    for (int x =1; x<=rows; x++) {
+        for (int y = 1; y<=x; y++)
             cout << "*";
         cout << endl;
     }
