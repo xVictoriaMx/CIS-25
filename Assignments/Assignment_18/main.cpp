56b963f1b24b755b7b3a82fdb78a968d34491428
xVictoriaMx <156991042+xVictoriaMx@users.noreply.github.com>
2024-10-07 21:37:05 -0700

    Assignment 18

---
+++ Assignments/Assignment_18/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+#include "include/bubble_sort.hpp"
+
+int main() {
+    const int SIZE = 5;
+    int arr[SIZE] = {8, 2, 4, 1, 3}; 
+    cout << "Initial array: ";
+    for (int step = 0; step < SIZE; step++) {
+        cout << arr[step] << " ";
+    }
+    cout << endl;
+
+    bubbleSort(arr, SIZE);
+
+    cout << "Sorted array: ";
+    for (int step = 0; step < SIZE; step++) {
+        cout << arr[step] << " ";
+    }
+    cout << endl;
+
+    return 0;
+}
