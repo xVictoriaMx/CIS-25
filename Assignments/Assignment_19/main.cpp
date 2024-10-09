712ed9ac718ac36dc6a8b742f0451682179d8a4d
xVictoriaMx <156991042+xVictoriaMx@users.noreply.github.com>
2024-10-08 22:09:23 -0700

    Assignment 19

---
+++ Assignments/Assignment_19/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    int a = 5, b = 10;
+
+    int *p1;
+    int *p2;
+
+    p1 = &a; 
+    p2 = &b; 
+
+    cout << "\nBefore." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    *p1 = *p1 + *p2; 
+    *p2 = *p1 - *p2; 
+    *p1 = *p1 - *p2; 
+
+    cout << "\nAfter." << endl;
+    cout << "a = " << a << ", b = " << b << endl;
+
+    return 0;
+}
